59f47eebcde1fefe0ac93374021026d6f3d241f0
mominul <ulhouqemomin39@gmail.com>
2023-11-04 21:36:57 +0600

    remove unnessary file

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ImageGallery from '../ImageGallery/ImageGallery';
-import I from '../ImageGallery/I';
-
 
 const Home = () => {
     return (
         <div>
             <ImageGallery />
-            <I />
         </div>
     );
 };
